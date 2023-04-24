1f3c5fa5ea6f1442fcbfa90233c43047fc450e10
nunsanity <manund03@uia.no>
2023-04-24 10:46:38 +0200

    dekrypter added

--- mycrypt/mycrypt.go
+++ mycrypt/mycrypt.go
@@ -15,6 +15,19 @@ func Krypter(melding []rune, alphabet []rune, chiffer int) []rune {
 	return kryptertMelding
 }
 
+func Dekrypter(kryptertMelding []rune, alphabet []rune, chiffer int) []rune {
+	dekryptertMelding := make([]rune, len(kryptertMelding))
+	for i := 0; i < len(kryptertMelding); i++ {
+		indeks := SokIAlfabetet(kryptertMelding[i], alphabet)
+		if indeks-chiffer < 0 {
+			dekryptertMelding[i] = alphabet[indeks-chiffer+len(alphabet)]
+		} else {
+			dekryptertMelding[i] = alphabet[indeks-chiffer]
+		}
+	}
+	return dekryptertMelding
+}
+
 func SokIAlfabetet(symbol rune, alfabet []rune) int {
 	for i := 0; i < len(alfabet); i++ {
 		if symbol == alfabet[i] {
